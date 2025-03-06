afcf27d16ab1c4a726f684df04de0bc45d0d235e
Raad <119070808+RAD1cal-Ctrl@users.noreply.github.com>
2025-03-06 12:45:06 +0000

    Player Creation

--- index.js
+++ index.js
@@ -7,13 +7,34 @@ canvas.height = 576
 c.fillStyle = 'white'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+//-----------------------------------------------MAP----------------------------------------------
 //c.drawiImage('/Img/map.png') passing a sting wont work as it it not on HTML
-
 const image = new Image()  //const image = new Image('/Img/map.png') not recognised by api
 image.src = '/Img/map.png'
 console.log(image)
 
+//-----------------------------------------------Player----------------------------------------------
+
+const playerImage = new Image()
+playerImage.src = '/Img/playerDown.png'
+
 
 image.onload = () => {  // without this the image is being called before HTML is loaded
-    c.drawImage(image, -100, -200)
+    c.drawImage(image, 0, -200)
+    c.drawImage(
+        playerImage,
+//  x cord for croping the player spread 
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+    
+//  Actual image that will be seen
+        265 - (playerImage.width / 4)/16 , 
+        250 - playerImage.height / 2,
+
+        playerImage.width / 4,
+        playerImage.height
+    )
 }
+
