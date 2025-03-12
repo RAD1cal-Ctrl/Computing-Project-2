a1d2f80101e00c9f38f20eb9da9c97eca9ac5f8a
Raad <119070808+RAD1cal-Ctrl@users.noreply.github.com>
2025-03-12 21:12:42 +0000

    clean up classes

--- index.js
+++ index.js
@@ -11,21 +11,6 @@ for (let i = 0; i < collisions.length; i += 70) {
 
 //-----------------------------------------------Boundary----------------------------------------------
 
-class Boundary {
-    static width = 48
-    static height = 48 
-    constructor({position}){
-        this.position = position
-        this.width = 48
-        this.height = 48
-    }
-
-    draw(){
-        c.fillStyle = 'rgba(255, 0, 0, 0)'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-}
-
 const boundaries = []
 const offset = {
     x: 0,
@@ -60,34 +45,7 @@ playerImage.src = '/Img/playerDown.png'
 //image.onload = () => {  // without this the image is being called before HTML is loaded
  
 //-----------------------------------------------Animation----------------------------------------------
-//this class will be ised to the screen can move the map with the character
-class Sprite{
-    constructor({position, image, frames = {max: 1} }) {
-        this.position = position
-        this.image = image
-        this.frames = frames
-        
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.max
-            this.height = this.image.height
-          }          
-    }
-    draw(){
-     //   c.drawImage(this.image, this.position.x, this.position.y)
-     c.drawImage(
-        this.image,
-        0, // source x
-        0, // source y
-        this.image.width / this.frames.max,
-        this.image.height,
-        this.position.x,
-        this.position.y,
-        this.image.width / this.frames.max,
-        this.image.height
-      )
-      
-    }
-}
+
 
 //  //  Actual image that will be seen
 //  265 - (this.image.width / 4)/16 , 
